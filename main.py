479cb737f5f037fb10a01b83a91841e3bcf3cac7
MikeMwai <mwai.michael@strathmore.edu>
2023-09-18 21:35:44 +0300

    email-generation-function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+# main.py
+
+# Import functions from the functions module
+from functions import email_generation
+
+# Generate email addresses for students
+email_addresses = email_generation
+
+
+
