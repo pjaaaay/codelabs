cf283c953cb327c50f5a4d42e9c8d4687b13c060
MikeMwai <mwai.michael@strathmore.edu>
2023-09-25 15:25:10 +0300

    update_main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Import functions from the functions module
 import functions
 
-# pip install openyxl efore running main.py
+# pip install openpyxl efore running main.py
 
 # Generate email addresses for students
 email_addresses = functions.generate_email
